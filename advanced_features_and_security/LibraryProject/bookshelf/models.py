2c1ed47f2595ef5eaf3b996e73766877070534f1
Amen Zelealem <amenzelealemtadese@gmail.com>
2025-03-02 19:22:11 +0300

    Added Custom Permisions

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+# Custom User Manager for creating and managing CustomUser
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None, date_of_birth=None, profile_photo=None):
         if not email:
@@ -18,6 +19,7 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+# Custom User Model
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to="profile_pics/", null=True, blank=True)
@@ -26,3 +28,22 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+# Book Model with Custom Permissions
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+
+    # Define Custom Permissions
+    class Meta:
+        permissions = [
+            ("can_create_book", "Can create book"),
+            ("can_edit_book", "Can edit book"),
+            ("can_delete_book", "Can delete book"),
+            ("can_view_book", "Can view book"),
+        ]
+
+    def __str__(self):
+        return self.title
