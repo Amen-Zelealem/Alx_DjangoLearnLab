db24c431eb2dc1ec57b0fe79a038e1ef2f0f0e9b
Amen Zelealem <amenzelealemtadese@gmail.com>
2025-03-09 19:15:42 +0300

    Updated Views

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
+["generics.ListAPIView"]
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()
