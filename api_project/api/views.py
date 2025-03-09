ba6e07b8f465c1095b91c5f9e2f9a427593611a4
Amen Zelealem <amenzelealemtadese@gmail.com>
2025-03-09 19:24:31 +0300

    Implementing CRUD Operations

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,13 @@
-from rest_framework.generics import ListAPIView
+# from rest_framework.generics import ListAPIView
+from rest_framework.generics import viewsets
 from .models import Book
 from .serializers import BookSerializer
-["generics.ListAPIView"]
 
-class BookList(ListAPIView):
+# class BookList(ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
