946c82185dcf94b4ae14a7e898cbe140953a2df4
Amen Zelealem <amenzelealemtadese@gmail.com>
2025-03-16 14:08:25 +0300

    added the django filter framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-
+from django_filters import rest_framework
 
 # ✅ ListView - Allows anyone to read, but only authenticated users can create
 class ListView(generics.ListCreateAPIView):
