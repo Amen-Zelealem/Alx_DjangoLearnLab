d1e44bc6aaacc92b3c04afd1172a8946c7f4d6de
Amen Zelealem <amenzelealemtadese@gmail.com>
2025-03-16 13:40:11 +0300

    updated views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,24 +2,12 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 
-# List all books (GET) and Create a new book (POST)
-class BookListCreateView(generics.ListCreateAPIView):
+class BookListCreateView(generics.ListCreateAPIView): 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    # Restrict Create to authenticated users
-    def get_permissions(self):
-        if self.request.method == "POST":
-            return [permissions.IsAuthenticated()]
-        return [permissions.AllowAny()]
-
-# Retrieve, Update, or Delete a book by ID
-class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView): 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-    # Restrict Update & Delete to authenticated users
-    def get_permissions(self):
-        if self.request.method in ["PUT", "DELETE"]:
-            return [permissions.IsAuthenticated()]
-        return [permissions.AllowAny()]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
