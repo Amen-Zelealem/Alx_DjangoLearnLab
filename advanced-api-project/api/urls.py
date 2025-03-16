c083d178d72f9f09c3ece8b47e4aa3e6551bbd9c
Amen Zelealem <amenzelealemtadese@gmail.com>
2025-03-16 13:47:20 +0300

    Fixed Update and Delete Urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path("books/", ListView.as_view(), name="book-list"),
     path("books/create/", CreateView.as_view(), name="book-create"),
     path("books/<int:pk>/", RetrieveView.as_view(), name="book-retrieve"),
-    path("books/<int:pk>/update/", UpdateView.as_view(), name="book-update"),
-    path("books/<int:pk>/delete/", DeleteView.as_view(), name="book-delete"),
+    path("books/update/<int:pk>/", UpdateView.as_view(), name="book-update"),
+    path("books/delete/<int:pk>/", DeleteView.as_view(), name="book-delete"),
     path("books/detail/<int:pk>/", DetailView.as_view(), name="book-detail"),
 ]
