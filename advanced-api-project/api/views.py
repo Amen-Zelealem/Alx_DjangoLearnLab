9c1df2169ab4ccf8acbd4d9539fc06f51d5abfad
Amen Zelealem <amenzelealemtadese@gmail.com>
2025-03-16 13:36:57 +0300

    Set Up Generic Views for CRUD Operations

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+# List all books (GET) and Create a new book (POST)
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    # Restrict Create to authenticated users
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+# Retrieve, Update, or Delete a book by ID
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    # Restrict Update & Delete to authenticated users
+    def get_permissions(self):
+        if self.request.method in ["PUT", "DELETE"]:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
