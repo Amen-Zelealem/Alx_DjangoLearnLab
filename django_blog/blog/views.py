9bac8135f82ac22c9126fac735ca7ffb025a67f8
Amen Zelealem <amenzelealemtadese@gmail.com>
2025-03-23 20:36:37 +0300

    updated views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'blog/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'blog/logout.html'
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html', {'user': request.user})
